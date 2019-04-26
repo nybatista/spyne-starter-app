a26b63e4a4bba6df7978fd0c59fe6e9df5e9bb44
Frank Batista <nybatista@aol.com>
2019-04-25 21:14:21 -0400

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+import "../css/main.css";
+import {SpyneApp, ViewStream} from 'spyne';
+import goat from "imgs/goat.jpg";
+import Data from "data/download.json";
+
+
+const spyneApp = new SpyneApp();
+
+const imgUrl = "//spynejs.org.s3.amazonaws.com/test2/animals_0009.jpg";
+
+const app = new ViewStream({
+  tagName: 'main',
+  id: 'app'
+
+});
+
+app.appendToDom(document.body);
+
+var n = Data.results[0],name;
+console.log('data is ',n.name.first);
+
+
+
+app.appendView(new ViewStream({
+  tagName: 'h3',
+  data: 'HOLA PLANETA! '+n.name.first
+
+}));
+
+
+let img = new ViewStream({
+  tagName: 'img',
+  src: imgUrl,
+  width: 300
+
+});
+
+
+app.appendView(img);
---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+module.exports = (env)=>{
+  const path = require('path');
+  const webpack = require('webpack');
+  const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const CleanWebpackPlugin = require('clean-webpack-plugin');
+  const miniCssPlugin = new MiniCssExtractPlugin({
+    filename: 'css/main.css'
+  });
+
+  const cleanPlugin = new CleanWebpackPlugin();
+  const htmlPlugin = new HtmlWebpackPlugin({
+    title: 'spyne-starter'
+  });
+
+
+  return {
+    mode: 'development',
+
+    entry: {
+
+        index: "./src/js/index.js"
+    },
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+      contentBase: './dist',
+      publicPath: '/'
+    },
+
+    plugins: [miniCssPlugin, cleanPlugin, htmlPlugin],
+
+    output: {
+        filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it use publicPath in webpackOptions.output
+                publicPath: '../'
+              }
+            },
+            "css-loader"
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader : 'file-loader',
+              options: {
+                name: '[path][name].[ext]'
+
+              }
+            } ]
+
+
+        }/*,
+
+        {
+          test: /\.(json)$/,
+          use: [
+            {
+              loader : 'file-loader',
+              options: {
+                name(file) {
+                  console.log("FILE IS ",file);
+                  let dir = String(file).includes('/imgs/') === true ? '/static/imgs/' : '/static/data/';
+                  if (process.env.NODE_ENV === 'development') {
+                    return  dir+'[name].[ext]';
+                  }
+
+                  return dir+'[name].[ext]';
+                },
+              }
+            } ]
+
+
+        }*/
+
+
+      ]
+    },
+
+
+    resolve: {
+      alias: {
+        imgs: path.resolve(__dirname, 'src/static/imgs/'),
+        data: path.resolve(__dirname, 'src/static/data/')
+
+      },
+    }
+
+  }
+
+
+
+};
